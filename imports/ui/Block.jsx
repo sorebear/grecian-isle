0b355bfda183de77cc1d3b32b1772c8628ac0200
sorebear <sorenbaird@gmail.com>
2018-03-18 22:10:02 -0700

    Added more styles to player workers

--- imports/ui/Block.jsx
+++ imports/ui/Block.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 export default () => (
   <div className="block">
-    <div className="block-face block-side front" />
-    <div className="block-face block-side back" />
-    <div className="block-face block-side left" />
-    <div className="block-face block-side right" />
-    <div className="block-face top" />
     {/* <div className="block-face bottom" /> */}
+    <div className="block-face block-side front" />
+    {/* <div className="block-face block-side front-left block-side-diagonal" /> */}
+    <div className="block-face block-side left" />
+    {/* <div className="block-face block-side back-left block-side-diagonal" /> */}
+    <div className="block-face block-side back" />
+    {/* <div className="block-face block-side back-right block-side-diagonal" /> */}
+    <div className="block-face block-side right" />
+    {/* <div className="block-face block-side front-right block-side-diagonal" /> */}
+    <div className="block-face top" />
   </div>
 );
