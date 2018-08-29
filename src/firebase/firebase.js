1d6e296a9852b9c2deca228f1b14d3cd14c06d82
Soren Baird <sorenbaird@gmail.com>
2018-08-28 17:02:12 -0700

    Updated double quotes to single quotes in firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,21 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const prodConfig = {
-  apiKey: "AIzaSyBleN9k8hHVHCGFats4I41nHov25q3b6Zc",
-  authDomain: "grecian-isle.firebaseapp.com",
-  databaseURL: "https://grecian-isle.firebaseio.com",
-  projectId: "grecian-isle",
-  storageBucket: "grecian-isle.appspot.com",
-  messagingSenderId: "99254806555"
+  apiKey: 'AIzaSyBleN9k8hHVHCGFats4I41nHov25q3b6Zc',
+  authDomain: 'grecian-isle.firebaseapp.com',
+  databaseURL: 'https://grecian-isle.firebaseio.com',
+  projectId: 'grecian-isle',
+  storageBucket: 'grecian-isle.appspot.com',
+  messagingSenderId: '99254806555'
 };
 
 const devConfig = {
-  apiKey: "AIzaSyBleN9k8hHVHCGFats4I41nHov25q3b6Zc",
-  authDomain: "grecian-isle.firebaseapp.com",
-  databaseURL: "https://grecian-isle.firebaseio.com",
-  projectId: "grecian-isle",
-  storageBucket: "grecian-isle.appspot.com",
-  messagingSenderId: "99254806555"
+  apiKey: 'AIzaSyBleN9k8hHVHCGFats4I41nHov25q3b6Zc',
+  authDomain: 'grecian-isle.firebaseapp.com',
+  databaseURL: 'https://grecian-isle.firebaseio.com',
+  projectId: 'grecian-isle',
+  storageBucket: 'grecian-isle.appspot.com',
+  messagingSenderId: '99254806555'
 };
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
